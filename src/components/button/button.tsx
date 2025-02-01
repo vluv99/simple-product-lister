b5c1cbb9c559900bf1444cdfa7907aee42117a0b
vluv99 <v.luva99@gmail.com>
2025-02-02 00:18:05 +0100

    added Button variants

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,11 +1,20 @@
 import { FC } from "react";
+import {twMerge} from "tailwind-merge";
 
 type ButtonProps = {
-    label: string;
+    label: string,
+    extendClasses?: string | undefined,
+    variant?: 'default' | 'big';
 }
-export const Button:FC<ButtonProps> = ({label}) => {
+export const Button: FC<ButtonProps> = ({label, extendClasses, variant = 'default'}) => {
+    const buttonStyle = twMerge('bg-black text-white  text-center font-semibold rounded-full',
+        extendClasses,
+        variant === 'default' && 'text-[16px] py-2 w-full',
+        variant === 'big' && 'text-[28px] w-[50%] h-full content-center');
+
     return (
-        <div className='bg-black text-white text-[16px] text-center font-semibold py-2 w-full rounded-full'>
+        <div
+            className={buttonStyle}>
             <span className='px-4'>{label}</span>
         </div>
     );
